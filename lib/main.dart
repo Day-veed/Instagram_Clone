2d09ece3fe5401bff86afe5695921f1e59a00245
Day-veed <davidadekunle9@gmail.com>
2021-10-09 23:51:25 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 }
 
 
-/*/import 'package:firebase_auth/firebase_auth.dart';
+/*import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
